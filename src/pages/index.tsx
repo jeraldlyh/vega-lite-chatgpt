6ec688dd4016b68226d82399efa2815e3c1cce66
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-08 23:25:15 +0800

    feat: init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-black">
+      <span className="text-white font-bold text-3xl">Home</span>
+    </div>
+  );
+}
