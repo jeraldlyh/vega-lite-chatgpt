1b3184eaf082117106399e9bfd7f1fbd7e6b2270
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-08 23:30:28 +0800

    feat: install vega-lite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
+import { VegaLite, VisualizationSpec } from "react-vega";
+
+const spec: VisualizationSpec = {
+  width: 400,
+  height: 200,
+  mark: "bar",
+  encoding: {
+    x: { field: "a", type: "ordinal" },
+    y: { field: "b", type: "quantitative" },
+  },
+  data: { name: "table" }, // note: vega-lite data attribute is a plain object instead of an array
+};
+
+const barData = {
+  table: [
+    { a: "A", b: 28 },
+    { a: "B", b: 55 },
+    { a: "C", b: 43 },
+    { a: "D", b: 91 },
+    { a: "E", b: 81 },
+    { a: "F", b: 53 },
+    { a: "G", b: 19 },
+    { a: "H", b: 87 },
+    { a: "I", b: 52 },
+  ],
+};
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-black">
-      <span className="text-white font-bold text-3xl">Home</span>
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-black">
+      <span className="text-white font-bold text-3xl mb-5">Graph</span>
+      <VegaLite spec={spec} data={barData} />
     </div>
   );
 }
