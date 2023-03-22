0725c3cf96e8d5f0ae854b1d9cc4d3eadd33aafe
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-22 20:29:06 +0800

    perf: update bar chart utterances

--- src/common/utterances/bar.ts
+++ src/common/utterances/bar.ts
@@ -1,3 +1,4 @@
 export const BAR_CHART_UTTERANCES = [
-  "Based on the bar chart, from 2000 to 2023, Singapore's growth ranged from 0% to 100%.",
+  "Based on the bar chart, from 2000 to 2021, Singapore's electricity consumption ranged from 500 to 1000",
+  "Based on the bar chart, Singapore has the highest GDP value of 6000 dollars",
 ];
