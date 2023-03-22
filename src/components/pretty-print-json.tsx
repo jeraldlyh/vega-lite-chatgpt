265762b164c1cae5b472e5465a88aeacc58e2464
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-22 19:56:47 +0800

    perf: render errors

--- src/components/pretty-print-json.tsx
+++ src/components/pretty-print-json.tsx
@@ -1,15 +1,26 @@
+import { useEffect, useState } from "react";
+
 interface IProps {
   data: any;
 }
 
 export const PrettyPrintJson = ({ data }: IProps) => {
   console.log(data);
+
+  const [value, setValue] = useState<string>("");
+
+  useEffect(() => {
+    try {
+      const json = JSON.stringify(JSON.parse(data), null, 2);
+      setValue(json);
+    } catch (error) {
+      setValue(data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   if (!data) {
     return <span className="text-white italic">No JSON data</span>;
   }
-  return (
-    <pre className="overflow-x-auto">
-      {JSON.stringify(JSON.parse(data), null, 2)}
-    </pre>
-  );
+  return <pre className="overflow-x-auto">{value}</pre>;
 };
