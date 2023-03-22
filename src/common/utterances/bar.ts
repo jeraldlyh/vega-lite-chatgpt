ab5168d092732620374a73e24f7a7ed5527666cd
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-22 20:37:48 +0800

    perf: add utterances for common highlights

--- src/common/utterances/bar.ts
+++ src/common/utterances/bar.ts
@@ -1,4 +1,5 @@
 export const BAR_CHART_UTTERANCES = [
   "Based on the bar chart, from 2000 to 2021, Singapore's electricity consumption ranged from 500 to 1000",
   "Based on the bar chart, Singapore has the highest GDP value of 6000 dollars",
+  "Based on the bar chart, the y-axis represents the names of the countries",
 ];
--- src/common/utterances/pie.ts
+++ src/common/utterances/pie.ts
@@ -1,4 +1,5 @@
 export const PIE_CHART_UTTERANCES = [
   "Based on the pie chart, about 75% of the respondents mentioned that they agree to this survey",
   "Based on the pie chart, Singapore's GDP takes up 33% of the global economy",
+  "The legend shows 2 categories of cars",
 ];
