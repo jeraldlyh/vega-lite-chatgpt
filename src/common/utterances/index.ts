79c9e7c009492825f86b048bf9634b1050526e6e
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-22 20:34:03 +0800

    feat: create utterances for line chart

--- src/common/utterances/index.ts
+++ src/common/utterances/index.ts
@@ -1,9 +1,10 @@
 import { TGraph } from "../types";
 import { BAR_CHART_UTTERANCES } from "./bar";
+import { LINE_CHART_UTTERANCES } from "./line";
 import { PIE_CHART_UTTERANCES } from "./pie";
 
 export const UTTERANCE_MAPPING: Record<TGraph, string[]> = {
   bar: BAR_CHART_UTTERANCES,
   pie: PIE_CHART_UTTERANCES,
-  line: BAR_CHART_UTTERANCES,
+  line: LINE_CHART_UTTERANCES,
 };
---
+++ src/common/utterances/line.ts
@@ -0,0 +1,6 @@
+export const LINE_CHART_UTTERANCES = [
+  "Based on the line chart, the figures for Ukraine were consistently high throughout the years",
+  "Based on the line chart, from 1950 to 2010, let's look at the range from 15% to 87%",
+  "Based on the line chart, the legend shows 4 types of dishes",
+  "Based on the line chart, from 2010 to 2019, the figures for Singapore showed a significant improvement from 3% to 50%",
+];
