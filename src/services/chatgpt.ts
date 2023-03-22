1ee82b3f42b6e501c542768cd56d221e789671a7
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-22 19:23:07 +0800

    perf(chatgpt): refine main prompt

--- src/services/chatgpt.ts
+++ src/services/chatgpt.ts
@@ -25,21 +25,21 @@ const initialise = async (
     VQL is a custom query language defined in this model that allows us to specify highlighting in a 2D chart specified in Vega-Lite.
 
     VQL schema consists of the following attributes:
-    1. 'highlight': represents an enum value 'visual element' | 'x-axis' | 'y-axis' | 'legend'
+    1. 'highlight': represents an enum value 'visual element' | 'x-axis' | 'y-axis' | 'legend' | 'data range'
     2. 'in': represents the layer indicated by an array of enum values. The array always contains the value 'data layer' or 'base layer' as the first element where 'base layer' represents the axes, title and legend while the 'data layer' represents visual elements like points in a scatter plot and bars in bar charts and histograms. Depending on the utterance, infer the intention and choose either 'data layer' or 'base layer' as the first element in the aray. An additional element is then appended to the array based on a set of rule mappings. The set of rule mappings is defined as such for 'in' attribute.
         2a. scatter = point
         2b. histogram | bar = bar
         2c. heatmap = rect
         2d. line = line
         2e. pie = arc
-    3. 'where': object of predicates represented by key(s) such as 'AND' or 'OR'. Each predicate contains an array of objects of which an object represents an condition. There are 5 different conditions available: 
+    3. 'where': object of predicates represented by key(s) such as 'AND' or 'OR'. 'OR' is always chosen by default, unless 'AND' makes the VQL more efficient. Each predicate contains an array of objects of which an object represents an condition. There are 5 different conditions available: 
         3a. EQUAL
         3b. NOT EQUAL
         3c. BETWEEN
         3d. MORE THAN
         3e. LESS THAN
     
-        Return the response in a valid JSON object.
+    Return the response in a valid JSON object, where the "VQL" attribute in the JSON object can be either a single object or an array of objects depending if multiple distinct "highlight" is required for charts like line chart.
     `,
     },
   ];
