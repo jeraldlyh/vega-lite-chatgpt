26718233ed47ca2863baa0b80ea1dfce62e00403
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-26 22:45:25 +0800

    perf(line): refine prompts and added new utterances

--- src/common/prompts/line.ts
+++ src/common/prompts/line.ts
@@ -6,7 +6,7 @@ export const LINE_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
   /* -------------------------------------------------------------------------- */
   {
     role: "user",
-    content: `Convert the following text into a VQL schema: "Based on the line chart, let's take a look at the trends from X to Y"`,
+    content: `'X' and 'Y' are placeholder texts. Convert the following text into a VQL schema: "Let's take a look at the trends from X to Y in the line chart"`,
   },
   {
     role: "assistant",
@@ -17,18 +17,80 @@ export const LINE_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
                 "in": ["base layer"],
                 "where": {
                     "OR": [
-                        {"name": {"BETWEEN": ["X", "Y"]}}
+                        {"value": {"BETWEEN": ["X", "Y"]}}
                     ]
                 }
             }
         }`,
+  },
+  {
+    role: "user",
+    content: `'X' and 'Y' are placeholder texts. Convert the following text into a VQL schema: "The line chart tells us that the figures has decreased from X to Y"`,
+  },
+  {
+    role: "assistant",
+    content: `
+        {
+            "VQL": {
+                "highlight": "data range",
+                "in": ["base layer"],
+                "where": {
+                    "OR": [
+                        {"value": {"BETWEEN": ["X", "Y"]}}
+                    ]
+                }
+            }
+        }`,
+  },
+  {
+    role: "user",
+    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "We are able to tell from the line chart that X and Y has the highest Z"`,
+  },
+  {
+    role: "assistant",
+    content: `
+        {
+            "VQL": {
+                "highlight": "visual element",
+                "in": ["data layer", "line"],
+                "where": {
+                    "OR": [
+                        {"name": {"EQUAL": "X"}},
+                        {"name": {"EQUAL": "Y"}}
+                    ]
+                }
+            }
+            
+        }`,
+  },
+  {
+    role: "user",
+    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based off the line chart, X has the highest growth while Y has the lowest growth"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": [
+            {
+                "highlight": "visual element",
+                "in": ["data layer", "line"],
+                "where": {
+                    "OR": [
+                        {"name": {"EQUAL": "X"}},
+                        {"name": {"EQUAL": "Y"}}
+                    ]
+                }
+            }
+        ]
+    }`,
   },
   /* -------------------------------------------------------------------------- */
   /*                         MULTI CATEGORY HIGHLIGHTS                          */
   /* -------------------------------------------------------------------------- */
   {
     role: "user",
-    content: `Convert the following text into a VQL schema: "Based on the line chart, let's take a look at X with Y percent in Z"`,
+    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based on the line chart, let's take a look at X with Y percent in the year Z"`,
   },
   {
     role: "assistant",
@@ -40,7 +102,7 @@ export const LINE_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
             "where": {
                 "AND": [
                     {"name": {"EQUAL": "X"}},
-                    {"percentage": {"EQUAL": "Y"}},
+                    {"value": {"EQUAL": "Y"}},
                     {"year": {"EQUAL": "Z"}}
                 ]
             }
@@ -49,7 +111,7 @@ export const LINE_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
   },
   {
     role: "user",
-    content: `Convert the following text into a VQL schema: "Based on the line chart, from A to B, the figures for C showed a growth of D percent"`,
+    content: `'A', 'B', 'C' and 'D' are placeholder texts. Convert the following text into a VQL schema: "As shown in the line chart, from year A to B, the figures for C showed a growth of D percent"`,
   },
   {
     role: "assistant",
@@ -79,7 +141,7 @@ export const LINE_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
   },
   {
     role: "user",
-    content: `Convert the following text into a VQL schema: "Based on the line chart, from A to B, the figures for C, D, E and F increased"`,
+    content: `'A', 'B', 'C', 'D', 'E' and 'F' are placeholder texts. Convert the following text into a VQL schema: "From year A to B, the figures for C, D, E and F increased in the line chart"`,
   },
   {
     role: "assistant",
@@ -110,12 +172,107 @@ export const LINE_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
         ]
     }`,
   },
+  {
+    role: "user",
+    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "As seen in the line chart, X has increased from Y to Z"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": [
+            {
+                "highlight": "data range",
+                "in": ["base layer"],
+                "where": {
+                    "OR": [
+                        {"value": {"BETWEEN": ["Y", "Z"]}
+                    ]
+                }
+            },
+            {
+                "highlight": "visual element",
+                "in": ["data layer", "line"],
+                "where": {
+                    "OR": [
+                        {"name": {"EQUAL": "X"}}
+                    ]
+                }
+            }
+        ]
+    }`,
+  },
+  {
+    role: "user",
+    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based off the line chart, X and Y has the highest number of Z"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": [
+            {
+                "highlight": "visual element",
+                "in": ["data layer", "line"],
+                "where": {
+                    "OR": [
+                        {
+                            "AND": [
+                                {"name": {"EQUAL": "X"}},
+                                {"value": {"EQUAL": "Z"}}
+                            ]
+                        },
+                        {
+                            "AND": [
+                                {"name": {"EQUAL": "Y"}},
+                                {"value": {"EQUAL": "Z"}}
+                            ]
+                        }
+                    ]
+                }
+            }
+        ]
+    }`,
+  },
+  {
+    role: "user",
+    content: `'A', 'B', 'C', 'D' and 'E' are placeholder texts. Convert the following text into a VQL schema: "We are able to tell from the line chart that A and B has the highest C of D in year E"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "rect"],
+            "where": {
+                "OR": [
+                    {
+                        "AND": [
+                            {"name": {"EQUAL": "A"}},
+                            {"value": {"EQUAL": "D"}},
+                            {"year": {"EQUAL": "E"}}
+                        ]
+                    },
+                    {
+                        "AND": [
+                            {"name": {"EQUAL": "B"}},
+                            {"value": {"EQUAL": "D"}},
+                            {"year": {"EQUAL": "E"}}
+                        ]
+                    }
+                ]
+            }
+        }
+        
+    }`,
+  },
   /* -------------------------------------------------------------------------- */
   /*                              COMMON HIGHLIGHTS                             */
   /* -------------------------------------------------------------------------- */
   {
     role: "user",
-    content: `Convert the following text into a VQL schema: "Based on the line chart, the y-axis displays the X of Y"`,
+    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "The y-axis displays the X of Y in the line chart"`,
   },
   {
     role: "assistant",
@@ -130,7 +287,7 @@ export const LINE_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
   },
   {
     role: "user",
-    content: `Convert the following text into a VQL schema: "Based on the line chart, the legend represents X"`,
+    content: `Convert the following text into a VQL schema: "The legend represents X in the line chart below"`,
   },
   {
     role: "assistant",
--- src/common/utterances/line.ts
+++ src/common/utterances/line.ts
@@ -1,7 +1,10 @@
 export const LINE_CHART_UTTERANCES = [
-  "Based on the line chart, the figures for Germany were consistently high throughout the years",
-  "Based on the line chart, from 2000 to 2008, the figures for Belgium, France, Italy and Germany just went up gradually",
-  "Based on the line chart, from 1950 to 2010, let's look at the range from 15% to 87%",
-  "Based on the line chart, the legend shows 4 types of dishes",
+  "The GDP figures for Germany were consistently high throughout the years in the line chart",
+  "As seen in the line chart, from 2000 to 2008, the figures for Belgium, France, Italy and Germany just went up gradually",
+  "From 1950 to 2010, let's look at the line chart where it ranges from 15 to 87 percent",
+  "Seen in the line chart, the legend shows 4 types of dishes",
   "Based on the line chart, from 2010 to 2019, the figures for Singapore showed a significant improvement from 3% to 50%",
+  "Since the opening of a new outlet store, the sales has increased from 100 dollars to 1000 dollars as shown in the line chart",
+  "From the line chart, we can tell that Singapore and Malaysia has the highest number of COVID-19 cases of about 20,000 cases",
+  "Across the years in the line chart, apple has the highest sales while orange has the lowest",
 ];
