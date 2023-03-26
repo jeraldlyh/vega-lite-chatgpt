1cae93bd7ccae98d5be95c7f69aee394de886388
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-26 23:25:14 +0800

    perf(bar): refine prompts and added new utterances

--- src/common/prompts/bar.ts
+++ src/common/prompts/bar.ts
@@ -2,196 +2,8 @@ import { ChatCompletionRequestMessage } from "openai";
 
 export const BAR_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
   /* -------------------------------------------------------------------------- */
-  /*                          COUNTRY RANGE HIGHLIGHTS                          */
-  /* -------------------------------------------------------------------------- */
-  {
-    role: "user",
-    content: `'A', 'B', 'C' and 'D' are placeholder texts. Convert the following text into a VQL schema: "As shown in the bar chart, A exceeded B in all the C except D"`,
-  },
-  {
-    role: "assistant",
-    content: `
-    {
-        "VQL": {
-            "highlight": "visual element",
-            "in": ["data layer", "bar"],
-            "where": {
-                "OR": [
-                    {
-                        "AND": [
-                            {"type": {"EQUAL": "A"}},
-                            {"name": {"EQUAL": "D"}}
-                        ]
-                    },
-                    {
-                        "AND": [
-                            {"type": {"EQUAL": "B"}},
-                            {"name": {"EQUAL": "D"}}
-                        ]
-                    }
-                ]
-            }
-        }
-    }`,
-  },
-  {
-    role: "user",
-    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based on the bar chart, both X and Y are the Z"`,
-  },
-  {
-    role: "assistant",
-    content: `
-    {
-        "VQL": {
-            "highlight": "visual element",
-            "in": ["data layer", "bar"],
-            "where": {
-                "OR": [
-                    {"name": {"EQUAL": "X"}},
-                    {"name": {"EQUAL": "Y"}}
-                ]
-            }
-        }
-    }`,
-  },
-  {
-    role: "user",
-    content: `'X' and 'Y' are placeholder texts. Convert the following text into a VQL schema: "As shown in the bar chart, the largest value is X in Y"`,
-  },
-  {
-    role: "assistant",
-    content: `
-    {
-        "VQL": {
-            "highlight": "visual element",
-            "in": ["data layer", "bar"],
-            "where": {
-                "AND": [
-                    {"type": {"EQUAL": "X"}},
-                    {"name": {"EQUAL": "Y"}}
-                ]
-            }
-        }
-    }`,
-  },
-  /* -------------------------------------------------------------------------- */
-  /*                             X RANGE HIGHLIGHTS                             */
-  /* -------------------------------------------------------------------------- */
-  {
-    role: "user",
-    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based on the histogram, most X were between Y and Z"`,
-  },
-  {
-    role: "assistant",
-    content: `
-    {
-        "VQL": {
-            "highlight": "visual element",
-            "in": ["data layer", "bar"],
-            "where": {
-                "OR": [
-                    {"value": {"BETWEEN": ["Y", "Z"]}}
-                ]
-            }
-        }
-    }`,
-  },
-  {
-    role: "user",
-    content: `'A', 'B', 'C' and 'D' are placeholder texts. Convert the following text into a VQL schema: "As shown in the histogram, some bars that at the A percent range include B percent, C percent and D percent"`,
-  },
-  {
-    role: "assistant",
-    content: `
-    {
-        "VQL": {
-            "highlight": "visual element",
-            "in": ["data layer", "bar"],
-            "where": {
-                "OR": [
-                    {"value": {"EQUAL": "B"}},
-                    {"value": {"EQUAL": "C"}},
-                    {"value": {"EQUAL": "D"}}
-                ]
-            }
-        }
-    }`,
-  },
-  {
-    role: "user",
-    content: `'A', 'B', 'C' and 'D' are placeholder texts. Convert the following text into a VQL schema: "As shown in the histogram, some bars such as A percent, B percent and C percent represents D"`,
-  },
-  {
-    role: "assistant",
-    content: `
-    {
-        "VQL": {
-            "highlight": "visual element",
-            "in": ["data layer", "bar"],
-            "where": {
-                "OR": [
-                    {"value": {"EQUAL": "A"}},
-                    {"value": {"EQUAL": "B"}},
-                    {"value": {"EQUAL": "C"}}
-                ]
-            }
-        }
-    }`,
-  },
-  {
-    role: "user",
-    content: `'A', 'B', 'C', 'D' and 'E' are placeholder texts. Convert the following text into a VQL schema: "Based on the bar chart, from A to B, C ranged from D percent to E percent"`,
-  },
-  {
-    role: "assistant",
-    content: `
-    {
-        "VQL": {
-            "highlight": "visual element",
-            "in": ["data layer", "bar"],
-            "where": {
-                "AND": [
-                    {"name": {"EQUAL": "C"}},
-                    {"value": {"BETWEEN": ["A", "B"]}}
-                    {"value": {"BETWEEN": ["D", "E"]}},
-                ]
-            }
-        }
-    }`,
-  },
-  /* -------------------------------------------------------------------------- */
   /*                         SINGLE CATEGORY HIGHLIGHTS                         */
   /* -------------------------------------------------------------------------- */
-  {
-    role: "user",
-    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based on the bar chart, X and Y were the lowest Z"`,
-  },
-  {
-    role: "assistant",
-    content: `
-    {
-        "VQL": {
-            "highlight": "visual element",
-            "in": ["data layer", "bar"],
-            "where": {
-                "OR": [
-                    {
-                        "AND": [
-                            {"value": {"EQUAL": "X"}},
-                            {"type": {"EQUAL": "Z"}}
-                        ]
-                    },
-                    {
-                        "AND": [
-                            {"value": {"EQUAL": "Y"}},
-                            {"type": {"EQUAL": "Z"}}
-                        ]
-                    }
-                ]
-            }
-        }
-    }`,
-  },
   {
     role: "user",
     content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based on the bar graph, the lowest values are X and Y"`,
@@ -253,7 +65,7 @@ export const BAR_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
   },
   {
     role: "user",
-    content: `'A', 'B', 'C' and 'D' are placeholder texts. Convert the following text into a VQL schema: "The bar chart shows that there's only A number of B which have C less than D"`,
+    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based on the bar chart, both X and Y are the Z"`,
   },
   {
     role: "assistant",
@@ -264,7 +76,182 @@ export const BAR_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
             "in": ["data layer", "bar"],
             "where": {
                 "OR": [
-                    {"name": {"EQUAL": "X"}}
+                    {"name": {"EQUAL": "X"}},
+                    {"name": {"EQUAL": "Y"}}
+                ]
+            }
+        }
+    }`,
+  },
+  {
+    role: "user",
+    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based on the bar chart, most X were between Y and Z"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "OR": [
+                    {"value": {"BETWEEN": ["Y", "Z"]}}
+                ]
+            }
+        }
+    }`,
+  },
+  {
+    role: "user",
+    content: `'A', 'B', 'C', 'D' and 'E' are placeholder texts. Convert the following text into a VQL schema: "As shown in the bar chart, some bars that such as B, C and D represents E"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "OR": [
+                    {"value": {"EQUAL": "B"}},
+                    {"value": {"EQUAL": "C"}},
+                    {"value": {"EQUAL": "D"}}
+                ]
+            }
+        }
+    }`,
+  },
+  /* -------------------------------------------------------------------------- */
+  /*                        MULTIPLE CATEGORY HIGHLIGHTS                        */
+  /* -------------------------------------------------------------------------- */
+  {
+    role: "user",
+    content: `'A', 'B', 'C', 'D' and 'E' are placeholder texts. Convert the following text into a VQL schema: "Based on the bar chart, from year A to B, C ranged from D percent to E percent"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "AND": [
+                    {"name": {"EQUAL": "C"}},
+                    {"year": {"BETWEEN": ["A", "B"]}}
+                    {"value": {"BETWEEN": ["D", "E"]}},
+                ]
+            }
+        }
+    }`,
+  },
+  {
+    role: "user",
+    content: `'X' and 'Y' are placeholder texts. Convert the following text into a VQL schema: "As shown in the bar chart, the largest value is X in Y"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "AND": [
+                    {"type": {"EQUAL": "X"}},
+                    {"name": {"EQUAL": "Y"}}
+                ]
+            }
+        }
+    }`,
+  },
+  {
+    role: "user",
+    content: `'A', 'B', 'C' and 'D' are placeholder texts. Convert the following text into a VQL schema: "As shown in the bar chart, A exceeded B in all the C except D"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "OR": [
+                    {
+                        "AND": [
+                            {"type": {"EQUAL": "A"}},
+                            {"name": {"EQUAL": "D"}}
+                        ]
+                    },
+                    {
+                        "AND": [
+                            {"type": {"EQUAL": "B"}},
+                            {"name": {"EQUAL": "D"}}
+                        ]
+                    }
+                ]
+            }
+        }
+    }`,
+  },
+  {
+    role: "user",
+    content: `'X', 'Y', and 'Z' are placeholder texts. Convert the following text into a VQL schema: "Based on the bar chart, X and Y were the lowest Z"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "OR": [
+                    {
+                        "AND": [
+                            {"value": {"EQUAL": "X"}},
+                            {"type": {"EQUAL": "Z"}}
+                        ]
+                    },
+                    {
+                        "AND": [
+                            {"value": {"EQUAL": "Y"}},
+                            {"type": {"EQUAL": "Z"}}
+                        ]
+                    }
+                ]
+            }
+        }
+    }`,
+  },
+  {
+    role: "user",
+    content: `'A', 'B', 'C' and 'D' are placeholder texts. Convert the following text into a VQL schema: "The bar chart shows that A has a number of B which have C less than D"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "OR": [
+                    {
+                        "AND": [
+                            {"name": {"EQUAL": "B"}},
+                            {"value": {"EQUAL": "A"}}
+                        ]
+                    },
+                    {
+                        "AND": [
+                            {"name": {"EQUAL": "D"}},
+                            {"value": {"EQUAL": "A + C"}}
+                        ]
+                    }
                 ]
             }
         }
@@ -284,6 +271,86 @@ export const BAR_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
             "where": {
                 "OR": [
                     {"name": {"EQUAL": "X"}}
+                ]
+            }
+        }
+    }`,
+  },
+  {
+    role: "user",
+    content: `'X', 'Y', 'Z' are placeholder texts. Convert the following text into a VQL schema: "The bar chart shows that X has the lowest number of Y while Z has the lowest"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "OR": [
+                    {"name": {"EQUAL": "X"}},
+                    {"name": {"EQUAL": "Z"}}
+                ]
+            }
+        }
+    }`,
+  },
+  {
+    role: "user",
+    content: `'A', 'B', 'C', 'D' and 'E' are placeholder texts. Convert the following text into a VQL schema: "From the bar chart, A is the most B as it has the highest number of C while D is the least B with E"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "OR": [
+                    {
+                        "AND": [
+                            {"name": {"EQUAL": "A"}},
+                            {"value": {"EQUAL": "C"}}
+                        },
+                    ]
+                    {
+                        "AND": [
+                            {"name": {"EQUAL": "D"}}
+                            {"value": {"EQUAL": "E"}}
+                        ]
+                    }
+                ]
+            }
+        }
+    }`,
+  },
+  {
+    role: "user",
+    content: `'A', 'B', 'C' and 'D' are placeholder texts. Convert the following text into a VQL schema: "From the bar chart, A has B which is C more than D"`,
+  },
+  {
+    role: "assistant",
+    content: `
+    {
+        "VQL": {
+            "highlight": "visual element",
+            "in": ["data layer", "bar"],
+            "where": {
+                "OR": [
+                    {
+                        "AND": [
+                            {"name": {"EQUAL": "A"}},
+                            {"value": {"EQUAL": "B"}}
+                        },
+                    ]
+                    {
+                        "AND": [
+                            {"name": {"EQUAL": "D"}}
+                            {"value": {"EQUAL": "B + C"}}
+                        ]
+                    }
                 ]
             }
         }
@@ -316,7 +383,7 @@ export const BAR_CHART_PROMPTS: ChatCompletionRequestMessage[] = [
     content: `
     {
         "VQL": {
-            "highlight": "legend",
+            "highlight": "y-axis",
             "in": ["base layer"],
             "where": {}
         }
--- src/common/utterances/bar.ts
+++ src/common/utterances/bar.ts
@@ -1,7 +1,8 @@
 export const BAR_CHART_UTTERANCES = [
-  "Based on the bar chart, from 2000 to 2021, Singapore's electricity consumption ranged from 500 to 1000",
-  "Based on the bar chart, Singapore has the highest GDP value of 6000 dollars",
-  "Based on the bar chart, the y-axis represents the names of the countries",
-  "Based on the bar chart, the figures for Germany were consistently high throughout the years",
-  "Based on the bar chart, chipotle customers can and do order meals with fewer than 800 calories, such as a cheese-free burrito bowl",
+  "From 2000 to 2021, Singapore's electricity consumption ranged from 500 to 1000 in the bar chart",
+  "We can deduce that from the bar chart, Singapore has the highest GDP value of 6000 dollars",
+  "The y-axis represents the names of the countries in the bar chart",
+  "As seen in the bar chart, chipotle customers can and do order meals with fewer than 800 calories, such as a cheese-free burrito bowl",
+  "From the bar chart, we can tell that apple has the highest amount of sales while pear has the lowest",
+  "We can tell that iPhone has the 33 transactions which is 50 more than Android in the bar chart",
 ];
