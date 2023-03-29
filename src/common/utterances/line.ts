007fbf23fd5f20eff9e930ac806175d6cd10b521
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-29 20:09:24 +0800

    perf(line): create utterances for ground truths

--- src/common/utterances/line.ts
+++ src/common/utterances/line.ts
@@ -1,3 +1,6 @@
+/* -------------------------------------------------------------------------- */
+/*                               TEST UTTERANCES                              */
+/* -------------------------------------------------------------------------- */
 export const LINE_CHART_UTTERANCES = [
   "The GDP figures for Germany were consistently high throughout the years in the line chart",
   "As seen in the line chart, from 2000 to 2008, the figures for Belgium, France, Italy and Germany just went up gradually",
@@ -7,4 +10,20 @@ export const LINE_CHART_UTTERANCES = [
   "Since the opening of a new outlet store, the sales has increased from 100 dollars to 1000 dollars as shown in the line chart",
   "From the line chart, we can tell that Singapore and Malaysia has the highest number of COVID-19 cases of about 20,000 cases",
   "Across the years in the line chart, apple has the highest sales while orange has the lowest",
+];
+
+/* -------------------------------------------------------------------------- */
+/*                           GROUND TRUTH UTTERANCES                          */
+/* -------------------------------------------------------------------------- */
+export const GROUND_TRUTH_LINE_CHART_UTTERANCES = [
+  "Based on the line chart, from 2000 to 2008, the figures for the UK showed a considerable growth of 17%,",
+  "Based on the line chart, finally, the UK had by far the lowest percentage of women in its Parliament in 2000, at only around 3 percent. ",
+  "Based on the line chart, let's look at Belgium  in 2004.",
+  "Based on the line chart, now, let's look at the trends from 2008 to 2012.",
+  "Based on the line chart, the y-axis displays the percentages of female parliament members.",
+  "Based on the line chart, the figures for Germany were consistently high throughout the years.",
+  "Based on the line chart, from 2000 to 2008, the figures for Belgium, France, Italy and Germany just went up gradually. ",
+  "Based on the line chart, belgium had 23% in 2004.",
+  "Based on the line chart, the UK and Belgium had relatively closer numbers – 20% and 24% respectively – in 2008.",
+  "Based on the line chart, from 2004 to 2008, let's look at the range from 15% to 30%.",
 ];
