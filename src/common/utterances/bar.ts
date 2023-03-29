fbcfd18a8022905e797b378ab4ec78d4b4a2fd10
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-29 19:46:30 +0800

    perf(bar): update bar chart ground truth utterances

--- src/common/utterances/bar.ts
+++ src/common/utterances/bar.ts
@@ -14,10 +14,12 @@ export const TEST_BAR_CHART_UTTERANCES = [
 /*                           GROUND TRUTH UTTERANCES                          */
 /* -------------------------------------------------------------------------- */
 export const GROUND_TRUTH_BAR_CHART_UTTERANCES = [
-  "From 2000 to 2021, Singapore's electricity consumption ranged from 500 to 1000 in the bar chart",
-  "We can deduce that from the bar chart, Singapore has the highest GDP value of 6000 dollars",
-  "The y-axis represents the names of the countries in the bar chart",
-  "As seen in the bar chart, chipotle customers can and do order meals with fewer than 800 calories, such as a cheese-free burrito bowl",
-  "From the bar chart, we can tell that apple has the highest amount of sales while pear has the lowest",
-  "We can tell that iPhone has the 33 transactions which is 50 more than Android in the bar chart",
+  "Based on the bar chart, in addition, generation exceeded consumption in all the countries except Germany.",
+  "Based on the bar chart, the largest electricity value is production in China.",
+  "Based on the bar chart, the top values for electricity are 5398 and 5322 respectively.",
+  "Based on the bar chart, overall, China and the United States led in both electricity output and use.",
+  "Based on the bar chart, Brazil, Germany and Korea were the lowest producers of electricity.",
+  "Based on the bar chart, the lowest values for electricity are 449.5 and 455.8 respectively.",
+  "Based on the bar chart, there was only one country which had readings that were more than 5000.",
+  "Based on the bar chart, the legend indicates that this bar chart has two categories.",
 ];
