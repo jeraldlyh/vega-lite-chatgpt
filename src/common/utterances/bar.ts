9ed4bab035197b55c1ffb9a41fa20dafc8d74806
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-29 17:54:00 +0800

    perf(bar): create utterances for ground truths

--- src/common/utterances/bar.ts
+++ src/common/utterances/bar.ts
@@ -1,4 +1,19 @@
-export const BAR_CHART_UTTERANCES = [
+/* -------------------------------------------------------------------------- */
+/*                               TEST UTTERANCES                              */
+/* -------------------------------------------------------------------------- */
+export const TEST_BAR_CHART_UTTERANCES = [
+  "From 2000 to 2021, Singapore's electricity consumption ranged from 500 to 1000 in the bar chart",
+  "We can deduce that from the bar chart, Singapore has the highest GDP value of 6000 dollars",
+  "The y-axis represents the names of the countries in the bar chart",
+  "As seen in the bar chart, chipotle customers can and do order meals with fewer than 800 calories, such as a cheese-free burrito bowl",
+  "From the bar chart, we can tell that apple has the highest amount of sales while pear has the lowest",
+  "We can tell that iPhone has the 33 transactions which is 50 more than Android in the bar chart",
+];
+
+/* -------------------------------------------------------------------------- */
+/*                           GROUND TRUTH UTTERANCES                          */
+/* -------------------------------------------------------------------------- */
+export const GROUND_TRUTH_BAR_CHART_UTTERANCES = [
   "From 2000 to 2021, Singapore's electricity consumption ranged from 500 to 1000 in the bar chart",
   "We can deduce that from the bar chart, Singapore has the highest GDP value of 6000 dollars",
   "The y-axis represents the names of the countries in the bar chart",
--- src/common/utterances/index.ts
+++ src/common/utterances/index.ts
@@ -1,12 +1,32 @@
 import { TGraph } from "../types";
-import { BAR_CHART_UTTERANCES } from "./bar";
+import {
+  GROUND_TRUTH_BAR_CHART_UTTERANCES,
+  TEST_BAR_CHART_UTTERANCES,
+} from "./bar";
 import { LINE_CHART_UTTERANCES } from "./line";
 import { PIE_CHART_UTTERANCES } from "./pie";
 import { SCATTER_CHART_UTTERANCES } from "./scatter";
 
-export const UTTERANCE_MAPPING: Record<TGraph, string[]> = {
-  bar: BAR_CHART_UTTERANCES,
-  pie: PIE_CHART_UTTERANCES,
-  line: LINE_CHART_UTTERANCES,
-  scatter: SCATTER_CHART_UTTERANCES,
+interface ITestTruth {
+  TEST: string[];
+  GROUND_TRUTH: string[];
+}
+
+export const UTTERANCE_MAPPING: Record<TGraph, ITestTruth> = {
+  bar: {
+    TEST: TEST_BAR_CHART_UTTERANCES,
+    GROUND_TRUTH: GROUND_TRUTH_BAR_CHART_UTTERANCES,
+  },
+  pie: {
+    TEST: PIE_CHART_UTTERANCES,
+    GROUND_TRUTH: GROUND_TRUTH_BAR_CHART_UTTERANCES,
+  },
+  line: {
+    TEST: LINE_CHART_UTTERANCES,
+    GROUND_TRUTH: GROUND_TRUTH_BAR_CHART_UTTERANCES,
+  },
+  scatter: {
+    TEST: SCATTER_CHART_UTTERANCES,
+    GROUND_TRUTH: GROUND_TRUTH_BAR_CHART_UTTERANCES,
+  },
 };
