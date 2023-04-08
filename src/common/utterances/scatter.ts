560b96b92d5dc2ad90172498e127c916de1bfde8
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-04-08 21:52:57 +0800

    perf(scatter): refine prompts based on dataset

--- src/common/utterances/scatter.ts
+++ src/common/utterances/scatter.ts
@@ -1,3 +1,13 @@
-export const SCATTER_CHART_UTTERANCES = [
-  "Based on the scatter chart, we can look at the jobs with less than 60% women in 2010.",
+/* -------------------------------------------------------------------------- */
+/*                           GROUND TRUTH UTTERANCES                          */
+/* -------------------------------------------------------------------------- */
+export const GROUND_TRUTH_SCATTER_CHART_UTTERANCES = [
+  //   "Women were involved in 24% of computer programmer roles in 2010 and 28% in 1980",
+  //   "Points that are displayed below that line are jobs for which the proportion of women has decreased between 1980 and 2010",
+  //   "In both 1980 and 2010, jobs with over 60% women included a lot of social, humanitarian, and caregiving roles",
+  //   "The next jobs had from 50 to 60% female involvement in 2010",
+  //   "We can look at the jobs with less than 60% women in 2010",
+  "We can look at jobs with greater than 70% female involvement in 1980",
+  "The x-axis displays the percentage of women in the workplace in 1980",
+  //   "The legend displays the scale from large losses to large gains",
 ];
