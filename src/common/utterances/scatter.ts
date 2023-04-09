dcdb7e121b93915b58e8e26f401c15dfc2c95ab2
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-04-09 15:33:36 +0800

    perf(scatter): standardise prompts with other chart types in v1.0.0

--- src/common/utterances/scatter.ts
+++ src/common/utterances/scatter.ts
@@ -1,3 +1,10 @@
 export const SCATTER_CHART_UTTERANCES = [
-  "Based on the scatter chart, we can look at the jobs with less than 60% women in 2010.",
+  //   "Based on the scatter chart, women were involved in 24% of computer programmer roles in 2010 and 28% in 1980",
+  //   "Based on the scatter chart, points that are displayed below that line are jobs for which the proportion of women has decreased between 1980 and 2010",
+  "Based on the scatter chart, in both 1980 and 2010, jobs with over 60% women included a lot of social, humanitarian, and caregiving roles",
+  "Based on the scatter chart, the next jobs had from 50 to 60% female involvement in 2010",
+  "Based on the scatter chart, we can look at the jobs with less than 60% women in 2010",
+  "Based on the scatter chart, we can look at jobs with greater than 70% female involvement in 1980",
+  //   "Based on the scatter chart, the x-axis displays the percentage of women in the workplace in 1980",
+  //   "Based on the scatter chart, the legend displays the scale from large losses to large gains",
 ];
